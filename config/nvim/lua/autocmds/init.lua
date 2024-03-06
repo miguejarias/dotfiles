e9eb2e3a6203debd377df7c06f6bdf19aea5e98e
Miguel Arias <migue@Nuchis-MBP.attlocal.net>
2024-03-06 12:59:48 -0500

    nvim updates

--- config/nvim/lua/autocmds/init.lua
+++ config/nvim/lua/autocmds/init.lua
@@ -4,7 +4,4 @@ vim.cmd [[
         autocmd!
         autocmd CmdlineLeave : echo ''
     augroup end
-
-    "auto format any file with lsp formatter on save
-    "autocmd BufWritePre * lua vim.lsp.buf.format()
 ]]
